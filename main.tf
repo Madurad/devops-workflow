9efba76def0e6d08cbf354bdedd6f5e57488dbb7
Madura Dissanayake <madura.dissanayake@ukg.com>
2025-11-16 13:51:25 -0500

    set the disk size

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = "e2-small"
-    disk_size_gb = 20
+    disk_size_gb = var.disk_size_gb
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
