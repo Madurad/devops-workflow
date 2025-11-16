9acfb28e7e49f6a4a850185c906c47f57b85fa12
Madura Dissanayake <madura.dissanayake@ukg.com>
2025-11-15 22:57:11 -0500

    removed project services

--- main.tf
+++ main.tf
@@ -81,25 +81,6 @@ data "google_compute_zones" "available" {
   region = var.gcp_region
 }
 
-resource "google_project_service" "cloudresource_manager" {
-  service = "cloudresourcemanager.googleapis.com"
-  project = var.gcp_project_id
-}
-
-resource "google_project_service" "compute_api" {
-  service = "compute.googleapis.com"
-  project = var.gcp_project_id
-}
-
-resource "google_project_service" "gke_api" {
-  service  = "container.googleapis.com"
-  project  = var.gcp_project_id
-  depends_on = [
-    google_project_service.cloudresource_manager,
-    google_project_service.compute_api
-  ]
-}
-
 resource "google_container_cluster" "primary" {
   name     = "${var.project_name}-k8s-cluster"
   location = var.gcp_region
