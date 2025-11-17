cfa47efdf2d514fc0ba3c7c7720749b12b7052b5
Madura Dissanayake <madura.dissanayake@ukg.com>
2025-11-16 21:09:11 -0500

    added argocd.tf

---
+++ argocd.tf
@@ -0,0 +1,45 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+    host                   = google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  
+}
+
+provider "helm" {
+  kubernetes = {
+    host = google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token = data.google_client_config.default.access_token
+  }
+}
+
+resource "kubernetes_namespace" "argocd" {
+    metadata {
+        name = "argocd"
+    }
+  
+}
+
+resource "helm_release" "argocd" {
+
+    depends_on = [ kubernetes_namespace.argocd, google_container_node_pool.primary_nodes ]
+    name       = "argocd"
+    repository = "https://argoproj.github.io/argo-helm"
+    chart      = "argo-cd"
+    namespace  = kubernetes_namespace.argocd.metadata[0].name
+    version    = "5.23.6"
+    values = [
+        file("${path.module}/argocd-values.yaml")
+    ]
+
+    set = [ {
+      name = "server.service.type"
+      value = "LoadBalancer"
+    },
+    {
+      name = "server.service.loadBalancerIP"
+      value = ""
+    } ]
+}       
