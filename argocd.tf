8fd08527d3b01203e2bd98ad6a62151f1bc4a0af
Madura Dissanayake <madura.dissanayake@ukg.com>
2025-11-23 18:24:46 -0500

    added argocd.tf

--- argocd.tf
+++ argocd.tf
@@ -29,7 +29,7 @@ resource "helm_release" "argocd" {
     repository = "https://argoproj.github.io/argo-helm"
     chart      = "argo-cd"
     namespace  = kubernetes_namespace.argocd.metadata[0].name
-    version    = "5.23.6"
+    # version    = "5.23.6"
 
     set = [ {
       name = "server.service.type"
