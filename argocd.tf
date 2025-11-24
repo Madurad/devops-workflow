9613b53d59e112d5bd8e32c6fcff52ac90d2009b
Madura Dissanayake <madura.dissanayake@ukg.com>
2025-11-23 19:16:08 -0500

    troubleshooting

--- argocd.tf
+++ argocd.tf
@@ -8,27 +8,27 @@ output "access_token" {
   value = data.google_client_config.default.access_token
 }
 
-# provider "kubernetes" {
-#     host                   = google_container_cluster.primary.endpoint
-#     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-#     token                  = data.google_client_config.default.access_token
+provider "kubernetes" {
+    host                   = google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
   
-# }
+}
 
-# provider "helm" {
-#   kubernetes = {
-#     host = google_container_cluster.primary.endpoint
-#     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-#     token = data.google_client_config.default.access_token
-#   }
-# }
+provider "helm" {
+  kubernetes = {
+    host = google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token = data.google_client_config.default.access_token
+  }
+}
 
-# resource "kubernetes_namespace" "argocd" {
-#     metadata {
-#         name = "argocd"
-#     }
+resource "kubernetes_namespace" "argocd" {
+    metadata {
+        name = "argocd"
+    }
   
-# }
+}
 
 # resource "helm_release" "argocd" {
 
